8ca5e1f23268ebba4a43c6ae91d5a812ed96351a
eric <eric.jeangirard@recherche.gouv.fr>
2024-06-04 11:56:16 +0200

    new embeddings

--- project/server/main/embeddings.py
+++ project/server/main/embeddings.py
@@ -9,6 +9,8 @@ logger = get_logger(__name__)
 
 nlp = spacy.load("en_core_sci_scibert")
 model_multilingual = SentenceTransformer('distiluse-base-multilingual-cased-v1')
+model_small = SentenceTransformer('thenlper/gte-small')
+model_large = SentenceTransformer('thenlper/gte-large')
 
 def get_embeddings(embed_type, text, normalize=True):
     if not isinstance(text, str) or text is None or len(text) == 0:
@@ -17,6 +19,10 @@ def get_embeddings(embed_type, text, normalize=True):
         return get_scibert_embeddings(text, normalize)
     elif embed_type == 'multilingual':
         return get_multilingual_embeddings(text, normalize)
+    elif embed_type == 'small':
+        return get_small_embeddings(text, normalize)
+    elif embed_type == 'large':
+        return get_large_embeddings(text, normalize)
     else:
         logger.debug(f'unknown {embed_type} embedding type!')
 
@@ -33,4 +39,12 @@ def get_scibert_embeddings(text, normalize=True):
 def get_multilingual_embeddings(text, normalize=True):
     tokvecs = model_multilingual.encode(text, normalize_embeddings=normalize)
     assert(len(tokvecs) == 512)
+    return { 'embeddings': [float(e) for e in tokvecs] }
+
+def get_small_embeddings(text, normalize=True):
+    tokvecs = model_small.encode(text, normalize_embeddings=normalize)
+    return { 'embeddings': [float(e) for e in tokvecs] }
+
+def get_large_embeddings(text, normalize=True):
+    tokvecs = model_large.encode(text, normalize_embeddings=normalize)
     return { 'embeddings': [float(e) for e in tokvecs] }
